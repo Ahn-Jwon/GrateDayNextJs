bd587bc219e003e21c4783cb0dd9a4b8a5e008da
Ahn-Jwon <ajw0376@gmail.com>
2025-06-03 18:39:47 +0900

    TailwindCSS.ts 파일 추가 및 종속성 변경 * TailwindCSS.ts 4버전은 안되니 3버전으로 변경

--- grateday/app/(root)/layout.tsx
+++ grateday/app/(root)/layout.tsx
@@ -1,13 +1,15 @@
 // 헤더와 푸터
+import Header from "@/components/shared/hearder";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col">
-        
-      <main className="flex-1 wrapper">{children}</main>
+    <div className='flex h-screen flex-col'>
+       <Header /> 
+      <main className='flex-1 wrapper'>{children}</main>
     </div>
   );
 }
