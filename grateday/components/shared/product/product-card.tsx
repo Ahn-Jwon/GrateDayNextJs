b23cd5ac5f335ce4862ee37f201e0d7bba64e426
Ahn-Jwon <ajw0376@gmail.com>
2025-06-05 16:22:17 +0900

    메인 레이아웃  가격표시  수정

--- grateday/components/shared/product/product-card.tsx
+++ grateday/components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -16,18 +17,20 @@ const ProductCard = ({ product }: { product: any }) => {
           />
         </Link>
       </CardHeader>
-      <CardContent className='p-4 grid gap-4'>
+      <CardContent className="p-4 grid gap-4">
         <div className="text-xss">{product.brand}</div>
         <Link href={`/product/${product.slug}`}>
-        <h2 className="text-sm font-medium">{ product.name }</h2>
+          <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
         <div className="flex-between gap-4">
-            <p>{ product.rating } Stars</p>
-            { product.stock > 0  ? (
-                <p className="font-bold">{ product.price }</p>
-            ) : (
-                <p className="text-destructive">Out Of Stock</p>
-            ) }
+          <p>{product.rating} Stars</p>
+          {product.stock > 0 ? (
+            <ProductPrice
+              value={Number(product.price)}
+            />
+          ) : (
+            <p className="text-destructive">Out Of Stock</p>
+          )}
         </div>
       </CardContent>
     </Card>
