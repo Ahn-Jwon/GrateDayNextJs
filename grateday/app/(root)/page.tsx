fc999d5a4d1077e9a280c9d631715cd2a6c13f6a
Ahn-Jwon <ajw0376@gmail.com>
2025-06-05 13:51:26 +0900

    메인 샘플데이터 레이아웃 구성

--- grateday/app/(root)/page.tsx
+++ grateday/app/(root)/page.tsx
@@ -1,5 +1,17 @@
-const HomePage =  () => {
-  return <>GrateDay</>;
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
+const HomePage = () => {
+  console.log(sampleData);
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newet Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
- 
+
 export default HomePage;
