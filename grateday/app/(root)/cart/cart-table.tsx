00b1ebf79a0e057791a37652a07330ee4b444c11
Ahn-Jwon <ajw0376@gmail.com>
2025-07-26 16:38:01 +0900

    CartTable 작성중

--- grateday/app/(root)/cart/cart-table.tsx
+++ grateday/app/(root)/cart/cart-table.tsx
@@ -1,6 +1,13 @@
 'use client';
-
+import { useRouter } from "next/navigation"; // 라우터
+import { useToast } from "@/hooks/use-toast"; // 후크
+import { useTransition } from "react";  // 전환
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.action";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import { Cart } from "@/types";
+import Link from "next/link";
+import Image from "next/image";
+
 
 const CartTable = ({ cart }: {cart?: Cart}) => {
     return ( <>
--- grateday/app/(root)/cart/page.tsx
+++ grateday/app/(root)/cart/page.tsx
@@ -1,16 +1,20 @@
 import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.action";
 
 export const metadata = {
     title: 'Shopping Cart'
 }
 
-const CartPage = () => {
+const CartPage = async() =>  {
+    const cart = await getMyCart();
+
+
     // 이쪽 리턴 하단부분에 View가 들어간다고 생각하면됨 
     return ( 
         <>
-        <CartTable />
+        <CartTable cart={cart} />
         </>
     );
-}
+};
  
 export default CartPage;
