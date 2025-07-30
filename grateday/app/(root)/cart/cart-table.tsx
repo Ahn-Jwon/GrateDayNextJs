32869b3b8bb90d363e180d93c12097cd99770d4f
Ahn-Jwon <ajw0376@gmail.com>
2025-07-30 17:55:16 +0900

    Cart Ui

--- grateday/app/(root)/cart/cart-table.tsx
+++ grateday/app/(root)/cart/cart-table.tsx
@@ -1,56 +1,124 @@
-'use client';
+"use client";
 import { useRouter } from "next/navigation"; // 라우터
 import { useToast } from "@/hooks/use-toast"; // 후크
-import { useTransition } from "react";  // 전환
+import { useTransition } from "react"; // 전환
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.action";
 import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import { Cart } from "@/types";
 import Link from "next/link";
 import Image from "next/image";
-import {Table, TableBody, TableHead, TableHeader, TableRow, TableCell} from '@/components/ui/table'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
 
-const CartTable = ({ cart }: {cart?: Cart}) => {
-    const router = useRouter();
-    const { toast } = useToast();
-    const [isPending, startTransition] = useTransition();
+  // Cart is empty. <Link href='/'>Go Shopping</Link>  카드가 비어있을 경우
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flext items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await removeItemFromCart(
+                              item.productId
+                            );
 
+                            if (!res.success) {
+                              toast({
+                                variant: "destructive",
+                                description: res.message,
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Minus className="w-4 h-4 " />
+                        )}
+                      </Button>
+                      <span>{item.qty}</span>
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await addItemToCart(item);
 
-// Cart is empty. <Link href='/'>Go Shopping</Link>  카드가 비어있을 경우
-    return ( <> 
-        <h1 className="py-4 h2-bold">Shopping Cart</h1>
-        { !cart || cart.items.length === 0 ? (
-            <div>
-                Cart is empty. <Link href='/'>Go Shopping</Link> 
-            </div>
-        ) : (
-            <div className="grid md:grid-cols-4 md:gap-5">
-                <div className="overflow-x-auto md:col-span-3">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>Item</TableHead>
-                                <TableHead className="text-center">Quantity</TableHead>
-                                <TableHead className="text-right">Price</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            { cart.items.map((item) => (
-                                <TableRow key={ item.slug}>
-                                    <TableCell>
-                                        <Link href={`/product/${item.slug}`} className="flext items-center">
-                                        <Image src={item.image} alt={item.name} width={50} height={50} />
-                                        <span className="px-2">{item.name}</span>
-                                        </Link>
-                                    </TableCell>
-                                </TableRow>
-                            )) }
-                        </TableBody>
-                    </Table>
-                </div>
-            </div>
-        ) }
-    </> );
-}
- 
+                            if (!res.success) {
+                              toast({
+                                variant: "destructive",
+                                description: res.message,
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Plus className="w-4 h-4 " />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
 export default CartTable;
