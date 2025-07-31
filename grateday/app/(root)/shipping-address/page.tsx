cf0cfc5548b34f794c4175ff948072621131af7c
Ahn-Jwon <ajw0376@gmail.com>
2025-07-31 17:46:52 +0900

    배송주소기능 추가중

---
+++ grateday/app/(root)/shipping-address/page.tsx
@@ -0,0 +1,33 @@
+import { auth } from "@/auth";
+import { getMyCart } from "@/lib/actions/cart.action";
+import { getUserById } from "@/lib/actions/user.actions";
+import { Metadata } from "next";
+import { reditect } from 'next/navigation';
+import { ShippingAddress } from "@/types";
+import ShippingAddressForm from "./shipping-address-form"; 
+
+
+export const metadata: Metadata = {
+    title: 'Shipping Address'
+}
+
+const ShippingAddressPage = async () => {
+    const cart = await getMyCart();
+
+    if(!cart || cart.items.length === 0) reditect('/cart');
+
+    const session = await auth();
+
+    const userId = session?.user?.id;
+
+    if (!userId) throw new Error('No user Id');
+
+    const user = await getUserById(userId);
+
+
+    return ( <>
+        <ShippingAddressForm address={ user.address as ShippingAddress}/>
+    </> );
+}
+ 
+export default ShippingAddressPage;
