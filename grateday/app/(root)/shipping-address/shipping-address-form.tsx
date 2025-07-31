eef08277a3d4039ce61d2ef26cc511c7218d51f0
Ahn-Jwon <ajw0376@gmail.com>
2025-07-31 18:46:41 +0900

    Cart 배송페이지 작성완료

--- grateday/app/(root)/shipping-address/shipping-address-form.tsx
+++ grateday/app/(root)/shipping-address/shipping-address-form.tsx
@@ -6,7 +6,7 @@ import { useToast } from "@/hooks/use-toast";
 import { useTransition } from "react";
 import { shippingAddressSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ControllerRenderProps, Field, useForm } from "react-hook-form";
+import { ControllerRenderProps, useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { shippingAddressDefaultValues } from "@/lib/constants";
 import {
@@ -18,20 +18,39 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+import { updateUserAdderss } from "@/lib/actions/user.actions";
 
+
+
+// || shippingAddressDefaultValues, << 이것이 없으면 비어있는 주소로 들어감
 const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
   const router = useRouter();
   const { toast } = useToast();
 
   const form = useForm<z.infer<typeof shippingAddressSchema>>({
     resolver: zodResolver(shippingAddressSchema),
-    defaultValues: address 
+    defaultValues: address || shippingAddressDefaultValues
   });
 
   const [isPending, startTransition] = useTransition();
 
-  const onSubmit = () => {
-    return;
+  const onSubmit:SubmitHandler<z.infer<typeof shippingAddressSchema>> = async (values) => {
+    
+    startTransition( async () => {
+        const res = await updateUserAdderss(values);
+
+        if(!res.success) {
+            toast({
+                variant: 'destructive',
+                description: res.message
+            });
+            return;
+        }
+
+        router.push('/payment-method');
+    })
   }
 
   return (
@@ -113,6 +132,60 @@ const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
               )}
             />
           </div>
+          <div className="flex flex-col md:flex-row gap-5">
+          <FormField
+            control={form.control}
+            name="postalCode"
+            render={({
+              field,
+            }: {
+              field: ControllerRenderProps<
+                z.infer<typeof shippingAddressSchema>,
+                "postalCode"
+              >;
+            }) => (
+              <FormItem className="min-w-full">
+                <FormLabel>Postal Code</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter postal Code" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex flex-col md:flex-row gap-5">
+        <FormField
+          control={form.control}
+          name="country"
+          render={({
+            field,
+          }: {
+            field: ControllerRenderProps<
+              z.infer<typeof shippingAddressSchema>,
+              "country"
+            >;
+          }) => (
+            <FormItem className="min-w-full">
+              <FormLabel>Country</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter country" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+      <div className="flex gap-2">
+          <Button type="submit" disabled={isPending}>
+            { isPending ? (
+                <Loader className='w-4 h-4 animate-spin' />
+            ) : (
+                <ArrowRight className='w-4 h-4' />
+            ) }{' '}
+            Continue
+          </Button>
+      </div>
           </form>
         </Form>
       </div>
