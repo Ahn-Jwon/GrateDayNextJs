9ad2ceeb0ab01a7a1624f274b8619d57ceb5eb51
Ahn-Jwon <ajw0376@gmail.com>
2025-07-31 23:01:33 +0900

    Cart 수정

--- grateday/auth.ts
+++ grateday/auth.ts
@@ -85,39 +85,58 @@ export const config = {
                 });
              }
 
-             if (trigger === 'signIn' || trigger === 'signUp') {
+             if (['signIn', 'signUp'].includes(trigger)) {
                 const cookiesObject = await cookies();
                 const sessionCartId = cookiesObject.get('sessionCartId')?.value;
-
-                if (sessionCartId) {
-                    const sessionCart = await prisma.cart.findFirst({
-                        where: { sessionCartId }
-                    });
-
-                    if(sessionCart) {
-                        // Delete current user cart
-                        await prisma.cart.deleteMany({
-                            where: { userId: user.id },
-                        });
-
-                        // Assign new cart
-                        await prisma.cart.update({ 
-                            where: {id: sessionCart.id },
-                            data: {userId: user.id}
-                         })
-                    }
+       
+                if (!sessionCartId) return;
+       
+                const sessionCart = await prisma.cart.findFirst({
+                  where: { sessionCartId },
+                });
+       
+                if (!sessionCart) return;
+       
+                if (sessionCart.userId !== user.id) {
+                  // Delete current user cart
+                  await prisma.cart.deleteMany({
+                    where: { userId: user.id },
+                  });
+       
+                  // Assign new cart
+                  await prisma.cart.update({
+                    where: { id: sessionCart.id },
+                    data: { userId: user.id },
+                  });
                 }
-             }
+              }
         }
 
         // Handle session updates
-        if (session?.user.name && trigger === 'update') {
-            token.name = session.user.name;
-        }
+        // if (session?.user.name && trigger === 'update') {
+        //     token.name = session.user.name;
+        // }
 
         return token;
     },
     authorized({ request, auth }: any) {
+        // Array of regex patterns of paths we want to protect
+        const protectedPaths = [
+            /\/shipping-address/,
+            /\/payment-method/,
+            /\/place-order/,
+            /\/profile/,
+            /\/user\/(.*)/,
+            /\/order\/(.*)/,
+            /\/admin/,
+        ];
+
+        // Get pathname from the req URL object
+        const { pathname } = request.nextUrl;
+
+        // Check if user is not authenticated and accessing a protected path
+        if(!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
         // Chack forr session cart cookie 
         if (!request.cookies.get('sessionCartId')) {
             // Generate new session cart id cookie
