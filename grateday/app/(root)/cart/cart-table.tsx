27762ec32f3f2e3ec28b86e9a876521b88e58eea
Ahn-Jwon <ajw0376@gmail.com>
2025-07-31 11:59:12 +0900

    Cart Ui 가격노출 Total

--- grateday/app/(root)/cart/cart-table.tsx
+++ grateday/app/(root)/cart/cart-table.tsx
@@ -16,6 +16,8 @@ import {
   TableCell,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -115,6 +117,17 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
               </TableBody>
             </Table>
           </div>
+
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <div className="pb-3 text-xl">
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                <span className="font-bold">
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
--- grateday/lib/utils.ts
+++ grateday/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -47,5 +48,22 @@ export function round2(value: number | string) {
     return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
   } else {
     throw new Error('Value is not a number or string')
+  }
+}
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 2
+});
+
+// Format currency using the formatter above
+export function formatCurrency(amount: number | string | null ) {
+  if (typeof amount === 'number') {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if(typeof amount === 'string') {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return 'NaN';
   }
 }
