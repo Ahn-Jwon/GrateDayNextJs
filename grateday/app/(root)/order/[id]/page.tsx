0ae94202b35101837cc294011e85d92100610352
Ahn-Jwon <ajw0376@gmail.com>
2025-08-06 19:00:51 +0900

    Order detail 작성중

--- grateday/app/(root)/order/[id]/page.tsx
+++ grateday/app/(root)/order/[id]/page.tsx
@@ -1,4 +1,23 @@
-const OrderDetailsPage = () => {
+import { Metadata } from "next";
+import { getOrderById } from "@/lib/actions/order.actions";
+import { notFound } from "next/navigation";
+import { ShippingAddress } from "@/types";
+
+export const metadata: Metadata = {
+    title: 'Order Details',
+}
+
+const OrderDetailsPage = async (props: {
+    params: Promise<{
+        id: string
+    }>
+}) => {
+
+    const { id } = await props.params;
+
+    const order = await getOrderById(id);
+    if (!order) notFound();
+
     return <>Details</>;
 }
  
