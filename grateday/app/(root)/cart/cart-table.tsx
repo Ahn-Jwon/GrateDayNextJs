d1a0f8b7a4371290b3767b0d9627fb2e3ec6e4a1
Ahn-Jwon <ajw0376@gmail.com>
2025-08-10 15:52:11 +0900

    paypal Test Token

--- grateday/app/(root)/cart/cart-table.tsx
+++ grateday/app/(root)/cart/cart-table.tsx
@@ -122,7 +122,7 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
               <div className="pb-3 text-xl">
                 Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
                 <span className="font-bold">
-                  {formatCurrency(cart.taxPrice)}
+                  {formatCurrency(cart.itemsPrice)}
                 </span>
               </div>
               <Button className="w-full" disabled={isPending} onClick={ () => startTransition(() => router.push('/shipping-address'))}>
