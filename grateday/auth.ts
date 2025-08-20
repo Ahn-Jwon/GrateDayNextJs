4f56abd6459700577e6e55c65bb50201c42a3d2b
Ahn-Jwon <ajw0376@gmail.com>
2025-08-20 14:14:27 +0900

    Mypage 작성완료 추후 컨텐츠 추가

--- grateday/auth.ts
+++ grateday/auth.ts
@@ -111,11 +111,11 @@ export const config = {
                 }
               }
         }
-
-        // Handle session updates
-        // if (session?.user.name && trigger === 'update') {
-        //     token.name = session.user.name;
-        // }
+        
+        // Handle session updateds
+        if(session?.user.name && trigger === 'update') { 
+            token.name = session.user.name;
+         }
 
         return token;
     },
