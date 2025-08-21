adc3bdf56b55231ae84525653d5ffad8c56a3ad2
Ahn-Jwon <ajw0376@gmail.com>
2025-08-21 22:34:55 +0900

    Admin page action 작성중

--- grateday/lib/actions/order.actions.ts
+++ grateday/lib/actions/order.actions.ts
@@ -12,6 +12,7 @@ import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { string } from "zod";
 import { PAGE_SIZE } from "../constants";
+import { Prisma } from "@prisma/client";
 
 // Create order and create the order items
 
@@ -284,4 +285,48 @@ export async function approvePayPalOrder(
             data,
             totalPages: Math.ceil(dataCount / limit),
         }
+     }
+
+     type SalesDataType = {
+        month: string;
+        totalSales: number;
+     } [];
+
+     //Get sales data and order summary
+     export async function getOrderSummary() {
+        // Get counts for each resource
+        const ordersCount = await prisma.order.count();
+        const productCount = await prisma.product.count();
+        const userCount = await prisma.user.count();
+
+        // Calculate the total sales 
+        const totalSales = await prisma.order.aggregate({
+            _sum: { totalPrice: true }
+        });
+
+        // Get monthly sales 
+        const salesDataRaw = await prisma.$queryRaw<Array<{month: string; totalSales: Prisma.Decimal}>>`SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales" FROM "Order" GROUP BY to_char("createdAt", 'MM/YY')`
+
+        const salesData:SalesDataType = salesDataRaw.map((entry) =>({
+            month: entry.month,
+            totalSales: Number(entry.totalSales),
+        }));
+
+        // Get latest sales
+        const latestSales = await prisma.order.findMany({
+            orderBy: { createdAt: 'desc' },
+            include: { 
+                user: { select: { name: true } }
+             },
+             take: 6
+        });
+
+        return {
+            ordersCount,
+            productCount,
+            userCount,
+            totalSales,
+            latestSales,
+            salesData,
+        }; 
      }
