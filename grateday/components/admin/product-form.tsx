517e3e79a094756e8bfb1bd9143f58684b067da2
Ahn-Jwon <ajw0376@gmail.com>
2025-08-29 19:05:55 +0900

    Image upload 작성중

--- grateday/components/admin/product-form.tsx
+++ grateday/components/admin/product-form.tsx
@@ -6,7 +6,7 @@ import { insertProductSchema, updateProductSchema } from "@/lib/validators";
 import { Product } from "@/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { ControllerRenderProps, useForm } from "react-hook-form";
+import { ControllerRenderProps, SubmitHandler, useForm } from "react-hook-form";
 import z from "zod";
 import {
   Form,
@@ -20,13 +20,14 @@ import slugify from "slugify";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+import { createProduct, updateProduct } from "@/lib/actions/product.actions";
 
 const ProductForm = ({
   type,
   product,
   productId,
 }: {
-  type: 'Create' | 'Update';
+  type: "Create" | "Update";
   product?: Product;
   productId?: string;
 }) => {
@@ -42,9 +43,51 @@ const ProductForm = ({
       product && type === "Update" ? product : productDefaultValues,
   });
 
+  const onSumit: SubmitHandler<z.infer<typeof insertProductSchema>> = async (values) => {
+    // On Create
+    if (type === 'Create') {
+      const res = await createProduct(values);
+
+      if(!res.success) { 
+        toast({
+          variant: 'destructive',
+          description: res.message
+        });
+      } else {
+        toast({
+          description: res.message
+        })
+        router.push('/admin/products');
+      }
+    }
+
+    // On Update
+    if (type === 'Update') {
+      if(!productId) {
+        router.push('/admin/products');
+        return;
+      }
+
+      const res = await updateProduct({ ...values, id: productId });
+
+      if(!res.success) { 
+        toast({
+          variant: 'destructive',
+          description: res.message
+        });
+      } else {
+        toast({
+          description: res.message
+        })
+        router.push('/admin/products');
+      }
+    }
+  }
+
+
   return (
     <Form {...form}>
-      <form className="space-y-8">
+      <form method="POST" onSubmit={form.handleSubmit(onSumit)} className="space-y-8">
         <div className="flex flex-col md:flex-row gap-5">
           {/* Name  */}
           <FormField
@@ -223,9 +266,14 @@ const ProductForm = ({
           />
         </div>
         <div>
-        <Button type="submit">
-            {form.formState.isSubmitting ? 'Submitting' : `${type} Product`}
-        </Button>
+          <Button
+            type="submit"
+            size="lg"
+            disabled={form.formState.isSubmitting}
+            className="button col-span-2 w-full"
+          >
+            {form.formState.isSubmitting ? "Submitting" : `${type} Product`}
+          </Button>
         </div>
       </form>
     </Form>
