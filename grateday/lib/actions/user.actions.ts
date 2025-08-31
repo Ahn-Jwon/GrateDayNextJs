3ff41fb977f0d99bcaa2923c46d915b3c00c8c25
Ahn-Jwon <ajw0376@gmail.com>
2025-08-31 17:14:56 +0900

    관리자페이지 유저관리 페이지 작성중

--- grateday/lib/actions/user.actions.ts
+++ grateday/lib/actions/user.actions.ts
@@ -13,6 +13,7 @@ import {prisma} from '@/db/prisma';
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
 import z from "zod";
+import { PAGE_SIZE } from "../constants";
 
 
 // Sign in the user with credentials 後で구글이나 깃허브로도 가능
@@ -169,3 +170,25 @@ export async function updateProfile(user: { name: string; email: string; }) {
         return { success: false, message: formatError(error)}
     }
 }
+
+// Get all the users
+export async function getAllUsers({ 
+    limit = PAGE_SIZE,
+    page
+ }: {
+    limit?: number;
+    page: number;
+ }) { 
+    const data = await prisma.user.findMany({
+        orderBy: { createdAt: 'desc' },
+        take: limit,
+        skip: (page - 1) * limit
+    });
+
+    const dataCount = await prisma.user.count();
+
+    return {
+        data,
+        totalPages: Math.ceil(dataCount / limit)
+    };
+ }
