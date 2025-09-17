e33cd2b39ef79ec42245a8aee3464ba0a9a86c9c
Ahn-Jwon <ajw0376@gmail.com>
2025-09-17 21:03:02 +0900

    Home 배너 작업

--- grateday/app/(root)/page.tsx
+++ grateday/app/(root)/page.tsx
@@ -1,15 +1,20 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProduct } from "@/lib/actions/product.actions";
+import {
+  getLatestProduct,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 const HomePage = async () => {
   const LatestProduct = await getLatestProduct();
+  const featuredProducts = await getFeaturedProducts();
+
   return (
     <>
-      <ProductList
-        data={LatestProduct}
-        title="New Item"
-        limit={4}
-      />
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+        )}
+      <ProductList data={LatestProduct} title="New Item" limit={4} />
     </>
   );
 };
